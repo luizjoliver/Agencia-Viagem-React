a74b4bae9ee07588c702686a41bb8a15d4ca8d42
luizjoliver <luizfellipejoliver14@gmail.com>
2025-05-28 13:42:09 -0300

    instalação libraries tailwind , adicionando assets projeto , criação constants , tipos do projeto e configurando root

--- app/root.tsx
+++ app/root.tsx
@@ -47,14 +47,14 @@ export default function App() {
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let message = "Oops!";
-  let details = "An unexpected error occurred.";
+  let details = "Um erro inesperado ocorreu";
   let stack: string | undefined;
 
   if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
+    message = error.status === 404 ? "404" : "Erro";
     details =
       error.status === 404
-        ? "The requested page could not be found."
+        ? "A página requerida não foi encontrada"
         : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
     details = error.message;
