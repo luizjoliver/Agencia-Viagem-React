68ba22d04236f193041ae3cf1a338549638b26f9
luizjoliver <luizfellipejoliver14@gmail.com>
2025-05-29 15:19:02 -0300

    Criação navBar mobile e pc

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,19 +1,21 @@
 import { Outlet } from "react-router";
 import { NavItems } from "~/components/NavItems";
 import {SidebarComponent} from "@syncfusion/ej2-react-navigations"
+import { MobileSideBar } from "~/components/MobileSideBar";
 
 export default function adminLayout() {
   return (
     <>
     <div className='admin-layout'>
-        mobileSideBar
+      
+       <MobileSideBar/>
 
       <aside className="w-full max-w-[270px] hidden lg:block">
         <SidebarComponent>
           <NavItems/>
         </SidebarComponent>
       </aside>
-      
+
       <aside className="children">
         <Outlet/>
       </aside>
